2e81353966ccfb981399cfe0ec35716ab8fd03a7
Jake Levy <levyj63@rowan.edu>
2020-08-12 15:34:34 +0000

    Strategy Pattern implemented in Pre-ES6 syntax

--- ActivityTracker/activityTracker.js
+++ ActivityTracker/activityTracker.js
@@ -4,27 +4,26 @@ var tracker = require("tracker");
 //distance is in miles
 //time is in minutes (note that tracker wants hours so you have a minor conversion to do)
 
-var activities = [ {activity: "walking", weight: 150, distance: 3, time: 45}, { activity: "running", weight: 200 , distance: 4, time: 40}, {activity: "running", weight: 175, distance: 5, time: 45}, {activity: "running", weight: 140, distance: 10, time: 240}, {activity: "walking", weight: 165, distance: 2, time: 20}, {activity: "walking", weight: 250, distance: 4, time: 75}, {activity: "running", weight: 215, distance: 3, time: 28}, {activity: "walking", weight: 220, distance: 5, time: 60}];
+var activities = [ {activity: "walking", weight: 150, distance: 3, time: 45}, { activity: "running", weight: 200 , distance: 4, time: 40}, {activity: "running", weight: 175, distance: 5, time: 45}, {activity: "running", weight: 140, distance: 10, time: 240}, {activity: "walking", weight: 165, distance: 2, time: 20}, {activity: "walking", weight: 250, distance: 4, time: 75}, {activity: "running", weight: 215, distance: 3, time: 28}, {activity: "walking", weight: 220, distance: 5, time: 60}, {activity: "swimming", weight: 160, distance: 5, time: 60}];
 
 
 for ( var act  in activities ) {
-   
-    timeHR = activities[act].time/60.0;
-    weight = activities[act].weight;
-    distance = activities[act].distance;
-    num=Number(act)+1;
-    console.log("Activity: " + num);
-    console.log(activities[act].activity);
-    console.log("Weight: " + weight +" lbs");
-    console.log("Speed: " + distance/timeHR + "miles per hour");
-    
-    if (activities[act].activity== "walking"){
-	caloriesBurned = tracker.calcWalkCal(weight, distance);
-	console.log("Calories Burned: "+ caloriesBurned );
-    }else{
-	caloriesBurned = tracker.calcRunCal(weight, distance);
-	console.log("Calories Burned: "+ caloriesBurned );
-    }
 
+    try{
+	exercise = activities[act].activity;
+	timeHR = activities[act].time/60.0;
+	weight = activities[act].weight;
+	distance = activities[act].distance;
+	var current= new tracker(exercise, weight, distance, timeHR);
+	num=Number(act)+1;
+	console.log("Activity: " + num);
+	console.log(exercise);
+	console.log("Weight: " + weight +" lbs");
+	console.log("Speed: " + current.calcSpeed() + "miles per hour");
+	caloriesBurned = current.calculate();
+	console.log("Calories Burned: "+ caloriesBurned );
+    } catch (err){
+	console.log(err.message);
+    }
     console.log("***************************");
 }
--- tracker/trackerService.js
+++ tracker/trackerService.js
@@ -1,8 +1,14 @@
 /*trackerService.js
 
-Provides basic Activity Tracker Functionality
+Provides Activity Tracker Functionality utilizing Strategy Design Pattern.
+Note that this file uses pre-ES6/prototyping syntax for defining a class.
+ While ES6 is a bit more firendly to people coming from Java and C++ and
+ other Class-based OO languages, this syntax is still valid and you *will*
+see it in the wild. And it should not scare you when you do.  
+
 Activities:  Walk/Run
-Input:  Distance (miles)
+Input:  Exercise type
+        Distance (miles)
         Weight (lbs)
         Time (hours)
 Output: Calories Burned
@@ -14,39 +20,59 @@ Output: Calories Burned
 */
 
 //requires weight in lbs
-
-var tracker = function Activity(exercise, weight, distance, time){
-    this.exercise = exercise;
-    this.weight = weight;
-    this.distance = distance;
-    this.time = time;
-}
-
-Activity.prototype = {
-    setExercise: function(exercise) {
-	this.exercise = exercise;
-    },
-
-    calculate: function(weight, distance) {
-	return this.exercise.calculate(weight, distance);
-    },
-    
-    //speed is whatever units the user wants to use
-    calcSpeed: function(distance, time){
-    return distance/time;
+//Time in hours
+//distance in miles
+//Constructor for the tracker Class.  
+var tracker = function(exercise, weight, distance, time) {
+    try{
+	if (exercise == "walking"){
+	    this.exercise = new walking();
+	} else if (exercise == "running") {
+	    this.exercise = new running();
+	} else {
+	    throw { message: "Unknown exercise!"};
+	}
+	this.weight = Number(weight);
+	this.distance = Number(distance);
+	this.time = Number(time);
+    } catch (err){
+	throw err;
     }
-}
+};
 
-var walking = function(weight, distance){
+
+//This calorie calculations for  walking and running are
+//implemented as Function Objects.  These can be assigned
+//to this.exercise at runtime.
+var walking = function(){
     this.calculate = function (weight, distance){
 	return 0.3 * weight * distance;
     }
 };
 //requires weight in lbs, and distance in miles
-var running  = function( weight, distance){
-    this.calculate = function calcRunCal(weight, distance){
+var running  = function(){
+    this.calculate = function (weight, distance){
     return 0.63 * weight * distance;
     }
+};
+//LAB ACTIVITY: I'd like you to improve the trackerService object by adding
+//some functionality to the prototype
+
+//This is the Class Prototype.  It is the "old" way of defining a JavaScript
+//Class.  It essentially provides functionality to all instances of tracker()
+//Note how this is defined like a JSON object (Parameter Name:  value). All JS
+//objects at their core, are basically JSON (obviously)
+tracker.prototype={    
+
+    calculate: function() {
+	return this.exercise.calculate(this.weight, this.distance);
+    },
+    
+    //speed is whatever units the user wants to use
+    calcSpeed: function(){
+	return this.distance/this.time;
+    }
+
 };
 
 module.exports = tracker;
