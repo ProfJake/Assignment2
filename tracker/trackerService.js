daad17c6dba3003306c126226a2cf55f143c19da
Jake Levy <levyj63@rowan.edu>
2020-08-12 17:09:20 +0000

    Added comments to pre-ES6

--- tracker/trackerService.js
+++ tracker/trackerService.js
@@ -4,7 +4,7 @@ Provides Activity Tracker Functionality utilizing Strategy Design Pattern.
 Note that this file uses pre-ES6/prototyping syntax for defining a class.
  While ES6 is a bit more firendly to people coming from Java and C++ and
  other Class-based OO languages, this syntax is still valid and you *will*
-see it in the wild. And it should not scare you when you do.  
+see it in the wild. And it should not scare you when you do. 
 
 Activities:  Walk/Run
 Input:  Exercise type
@@ -26,16 +26,27 @@ Output: Calories Burned
 var tracker = function(exercise, weight, distance, time) {
     try{
 	if (exercise == "walking"){
-	    this.exercise = new walking();
+	    this.exercise = new walking(); //exercise is a new instance of
+	                                  //of the function objects below
 	} else if (exercise == "running") {
 	    this.exercise = new running();
 	} else {
-	    throw { message: "Unknown exercise!"};
+	    //throwing errors is as simply as throwing an object literal
+	    //with a message property.
+	    throw { message: "Unknown exercise!"}; //if the exercise is unknown
 	}
+
+	//Note that, like constructors in Java/C++, nothing is being returned
+	//from the constructor.  But we have to use keyword this to indicate
+	//that we are createing an object here.
 	this.weight = Number(weight);
 	this.distance = Number(distance);
 	this.time = Number(time);
+
+	//catch any error thrown in object creation and re-throw it to
+	//calling module.
     } catch (err){
+	console.log("Error recieved during object creation");
 	throw err;
     }
 };
@@ -55,8 +66,10 @@ var running  = function(){
     return 0.63 * weight * distance;
     }
 };
-//LAB ACTIVITY: I'd like you to improve the trackerService object by adding
-//some functionality to the prototype
+//LAB ACTIVITY: I'd like you to improve the modularity of this code and get
+//used to pre-ES6 syntax.  Create a file for defining an "Exercise" called
+//Exercise.js.  Create a prototype for an Exercise, using the calculate
+//function as defined above.  Export both running and 
 
 //This is the Class Prototype.  It is the "old" way of defining a JavaScript
 //Class.  It essentially provides functionality to all instances of tracker()
@@ -64,15 +77,25 @@ var running  = function(){
 //objects at their core, are basically JSON (obviously)
 tracker.prototype={    
 
+
+    //runs the exercise specific calculation on internal variables
     calculate: function() {
 	return this.exercise.calculate(this.weight, this.distance);
     },
     
-    //speed is whatever units the user wants to use
+    //speed is consistently calculated for all exercise times (distance/time)
     calcSpeed: function(){
 	return this.distance/this.time;
     }
 
 };
 
+//Export statements expose data we want to be "importable". In this case
+//I want to export the tracker class (including its prototypes).  I don't
+//want to export the function objects because they are for internal use only.
+//They are assigned at creation.  They are analogous to inner classes.
+
+//Note that for improved modularity, I could have also defined the
+//exercise formulas in a separate file and exported them.  Then imported them
+//into this file.  I may choose that route in future iterations of the program.
 module.exports = tracker;
